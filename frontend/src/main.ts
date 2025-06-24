347498dd5c33ee4fec01dca104b45489fd8e52cf
Lixandro Beltre <lixandro.beltre@hotmail.com>
2025-06-24 14:22:40 -0400

    feat: implement login component and user authentication service

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import axios from 'axios';
+import { environment } from './environments/environment.development';
+
+axios.defaults.baseURL = environment.apiUrl;
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
