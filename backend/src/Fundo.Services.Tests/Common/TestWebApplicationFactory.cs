c9c90f89c2f2c52779e8f331412e73f27a1d832a
Lixandro Beltre <lixandro.beltre@hotmail.com>
2025-06-26 18:38:05 -0400

    fix: increase test timeout and configure in-memory connection string for tests

--- backend/src/Fundo.Services.Tests/Common/TestWebApplicationFactory.cs
+++ backend/src/Fundo.Services.Tests/Common/TestWebApplicationFactory.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Testcontainers.MsSql;
@@ -23,6 +25,14 @@ namespace Fundo.Services.Tests.Common
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    {"FundoLoan:ConnectionString", dbContainer.GetConnectionString()}
+                });
+            });
+
             builder.ConfigureTestServices(services =>
             {
                 var serviceDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(DbContextOptions<FundoLoanDbContext>));
